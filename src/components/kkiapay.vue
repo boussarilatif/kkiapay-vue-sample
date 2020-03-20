a349a7775f57e1ece6cdd89674bd132753ebb7ae
Boussari Latif <latif.boussari@mehriso.com>
2020-03-20 16:19:34 +0100

    first commit

---
+++ src/components/kkiapay.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <ul>
+      <a @click.prevent="openWidget">openKkiapayWidget</a>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  mounted() {},
+  methods: {
+    openWidget() {
+      window.openKkiapayWidget({
+        sandbox: true,
+        amount: "3000",
+        key: "f1e7270098f811e99eae1f0cfc677927"
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
